a0641c4b339f02da36155dd1485fffff737e2ac4
Grocky94 <lastleftdefence@gmail.com>
2023-07-27 14:41:43 +0530

    update

--- shoppersstop/src/Component/Navbar.jsx
+++ shoppersstop/src/Component/Navbar.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../Component/Navbar.css"
 const Navbar = () => {
     // state use for UI 
-    const [isshown, setIsshown] = useState(false);//login logout
-    const [iscategory, setIsCategory] = useState(false); //category 
+    const [isshown, setIsshown] = useState(false);//login logout modal
+    const [iscategory, setIsCategory] = useState(false); //category  dropwown
+    const [user, setUser] = useState({ name: '', email: '', password: '' }) // user register
+    console.log(user);
+    const [userLogin, setUserLogin] = useState({ email: '', password: "" }); // user login
+    console.log(userLogin)
+    // const router = useNavigate()
+    const isUserRegistered = JSON.parse(localStorage.getItem("Users")) !== null; //check register
+
+
 
     const openlogin = () => {
         setIsshown(true);//login logout
+        document.body.style.overflow = 'hidden';
 
     }
     const openCategory = () => {
@@ -15,12 +24,75 @@ const Navbar = () => {
     }
     const closelogin = () => {
         setIsshown(false);
+        document.body.style.overflow = 'unset';
     }
     const closeCategory = () => {
         setIsCategory(false);
     }
+    const handleChangeregister = (event) => {
+        setUser({ ...user, [event.target.name]: event.target.value })
+
+    }
+    const handleChangeLogin = (event) => {
+        setUserLogin({ ...userLogin, [event.target.name]: event.target.value })
+    }
 
 
+    const handleSubmitregister = (event) => {
+        event.preventDefault()
+        if (user.name && user.email && user.password) {
+            const array = JSON.parse(localStorage.getItem("Users")) || [];
+            const obj = { name: user.name, email: user.email, password: user.password }
+            array.push(obj);
+            localStorage.setItem("Users", JSON.stringify(array));
+            // setUser({ name: '', email: '', password: '' });
+            // setUserLogin({ email: '', password: '' });
+            alert("registration successful")
+            // router("/login")
+        } else {
+            alert("fill all credential first")
+        }
+    }
+    const handleSubmitLogin = (event) => {
+        event.preventDefault()
+
+        if (userLogin.email && userLogin.password) {
+            const user = JSON.parse(localStorage.getItem("Users"));
+            // const currentUser = JSON.parse(localStorage.getItem("Current-User"));
+            if (user) {
+                let flag = false;
+                for (let i = 0; i < user.length; i++) {
+                    if (user[i].email == userLogin.email && user[i].password == userLogin.password) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    return alert("please check the credential");
+                }
+                alert("login successful")
+                localStorage.setItem("Current-User", JSON.stringify(userLogin));
+                setIsshown(false)
+            } else {
+                alert("no records found")
+                window.confirm("need to register first")
+                // router("/register")
+            }
+
+        }
+        else {
+            alert("please submit all details")
+        }
+    }
+
+    const handleSubmitLogout = () => {
+        const currentUser = JSON.parse(localStorage.getItem("Current-User"));
+        if (currentUser) {
+            localStorage.removeItem("Current-User");
+            alert("user logged out");
+            setIsshown(false);
+        }
+    }
     return (
         <div>
             <nav>
@@ -35,7 +107,7 @@ const Navbar = () => {
                     </div>
                     <div id="navbar-search-login-area">
                         <div id="search-input-area">
-                            <input />
+                            <input placeholder='Search ' />
                             <div id="magni-icon">
                                 <img src="data:image/svg+xml,%0A%3Csvg width='26' height='26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M11.781 3.86a7.922 7.922 0 1 0 0 15.843 7.922 7.922 0 0 0 0-15.844zm-9.14 7.921a9.14 9.14 0 1 1 18.28 0 9.14 9.14 0 0 1-18.28 0z' fill='%23000' stroke='%23000' stroke-width='.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M17.382 17.383a.61.61 0 0 1 .862 0l4.936 4.936a.61.61 0 1 1-.862.862l-4.936-4.936a.61.61 0 0 1 0-.862z' fill='%23000' stroke='%23000' stroke-width='.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E%0A" alt="" />
                             </div>
@@ -46,8 +118,9 @@ const Navbar = () => {
                         <div className='navbar-icon-area'>
                             <i class="fa-solid fa-bag-shopping fa-xl"></i>
                         </div>
-                        <div className='navbar-icon-area' onMouseOver={openlogin}>
-                            <i class="fa-regular fa-user fa-xl"></i>
+                        <div className='navbar-icon-area' onMouseOver={openlogin} onClick={handleSubmitLogout} >
+                            {!userLogin.email? <i class="fa-regular fa-user fa-xl"></i> :
+                                <p style={{ color: "black" }}>{user?.name}logout</p>}
                         </div>
                     </div>
                 </div>
@@ -65,9 +138,9 @@ const Navbar = () => {
                 </div>}
             </nav>
             {/* entire screen  for login */}
-            {isshown? <div id="model">
+            {isshown ? <div id="model">
                 {/* small login screen   */}
-                <div id="sign-screen" >
+                {!isUserRegistered ? (< div id="sign-screen" >
                     {/* inside login black area  */}
                     <div id="banner-within-sign">
                         <div id="banner-sign-align">
@@ -75,23 +148,45 @@ const Navbar = () => {
                                 <p id="skip" onClick={closelogin}><i class="fa-solid fa-xmark fa-xl"></i></p>
                             </div>
                             <div>
-                                <h1 id="log-title">Login/Signup</h1>
+                                <h1 id="log-title">Signup</h1>
                                 <p id="log-sub-title">for Tailored experience</p>
                             </div>
                         </div>
                     </div>
                     {/* form area */}
                     <div id="login-form-area">
-                        <form>
-                            <div><input placeholder='Name' type="text" /></div>
-                            <div><input placeholder='Email' type="email" /></div>
-                            <div><input placeholder='Password' type="password" /></div>
-                            <input id="submit-btn" value="Continue" />
+                        <form onSubmit={handleSubmitregister}>
+                            <div><input placeholder='Name' name="name" type="text" onChange={handleChangeregister} value={user.name} /></div>
+                            <div><input placeholder='Email' name="email" type="email" onChange={handleChangeregister} value={user.email} /></div>
+                            <div><input placeholder='Password' name="password" type="password" onChange={handleChangeregister} value={user.password} /></div>
+                            <input id="submit-btn" type="submit" value="Continue" />
                         </form>
-                        <p id="terms-after-sign">By signing up you agree to our Terms of Service & Privacy Policy</p>
                     </div>
-                </div>
-            </div> : ""}
+                </div>) :
+                    (<div id="sign-screen" >
+                        {/* inside login black area  */}
+                        <div id="banner-within-sign">
+                            <div id="banner-sign-align">
+                                <div>
+                                    <p id="skip" onClick={closelogin}><i class="fa-solid fa-xmark fa-xl"></i></p>
+                                </div>
+                                <div>
+                                    <h1 id="log-title">Login</h1>
+                                    <p id="log-sub-title">for Tailored experience</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* form area */}
+                        <div id="login-form-area">
+                            <form onSubmit={handleSubmitLogin}>
+                                <div><input placeholder='Email' name="email" type="email" onChange={handleChangeLogin} value={userLogin.email} /></div>
+                                <div><input placeholder='Password' name="password" type="password" onChange={handleChangeLogin} value={userLogin.password} /></div>
+                                <input id="submit-btn" type="submit" value="Continue" />
+                            </form>
+                            <p id="terms-after-sign">By signing up you agree to our Terms of Service & Privacy Policy</p>
+                        </div>
+                    </div>)}
+            </div> : null}
         </div>
     )
 }
